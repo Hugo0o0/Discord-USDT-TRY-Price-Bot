1c379187dbee62a65ded7ab0a999ce8026ce0e68
Şükrü Tanrıverdi <hugohurleyy@gmail.com>
2024-05-14 15:59:00 +0300

    refactor:interval error

--- index.js
+++ index.js
@@ -9,30 +9,35 @@ client.once(Events.ClientReady, (readyClient) => {
   let latestPrice = 0;
   let errorCount = 0;
   setInterval(() => {
-    console.log(`Fetching new price...`);
-    console.log(`Latest price was ${latestPrice} TRY.`);
-    getPrice()
-      .then((data) => {
-        if (data.msg) {
-          errorCount++;
-          throw new Error(data.msg);
-        }
-        const parsedPrice = parseFloat(data.price).toFixed(2);
-        readyClient.guilds.cache.forEach(async (guild) => {
-          await guild.members.me.setNickname(`${parsedPrice} TRY`);
+    try {
+      console.log(`Fetching new price...`);
+      console.log(`Latest price was ${latestPrice} TRY.`);
+      getPrice()
+        .then((data) => {
+          if (data.msg) {
+            errorCount++;
+            throw new Error(data.msg);
+          }
+          const parsedPrice = parseFloat(data.price).toFixed(2);
+          readyClient.guilds.cache.forEach(async (guild) => {
+            await guild.members.me.setNickname(`${parsedPrice} TRY`);
+          });
+          latestPrice = parsedPrice;
+        })
+        .catch((err) => {
+          console.error(err);
+          if (errorCount === 10) {
+            console.error("Too many errors. Exiting...", err);
+            process.exit(1);
+          }
+          readyClient.guilds.cache.forEach(async (guild) => {
+            await guild.members.me.setNickname(`${latestPrice} TRY`);
+          });
         });
-        latestPrice = parsedPrice;
-      })
-      .catch((err) => {
-        console.error(err);
-        if (errorCount === 10) {
-          console.error("Too many errors. Exiting...", err);
-          process.exit(1);
-        }
-        readyClient.guilds.cache.forEach(async (guild) => {
-          await guild.members.me.setNickname(`${latestPrice} TRY`);
-        });
-      });
+    } catch (error) {
+      console.log("Interval Function error", error.message);
+      console.log(error);
+    }
   }, 10_000);
 });
 
